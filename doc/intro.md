e1612c567758a04d53d0e460697db080a7fd603c
Jacob Vallejo <you@jahkeup.com>
2014-07-09 13:54:41 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to monte-raft
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/monte_raft/core_test.clj
@@ -0,0 +1,7 @@
+(ns monte-raft.core-test
+  (:require [clojure.test :refer :all]
+            [monte-raft.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
