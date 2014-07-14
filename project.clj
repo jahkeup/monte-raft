98c15a317a6b896b9b80033d584bcbafeda0b9f3
Jacob Vallejo <you@jahkeup.com>
2014-07-14 12:05:30 -0400

    Clean up project and add deps

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/jahkeup/monte-raft"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[quickie "0.2.5"]]
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.zeromq/cljzmq "0.1.4"]
+                 [org.zeromq/jeromq "0.3.3"]
+                 [org.zeromq/cljzmq "0.1.4" :exclusions [org.zeromq/jzmq]]
                  [org.clojure/tools.logging "0.3.0"]
                  [org.clojure/data.json "0.2.5"]])
