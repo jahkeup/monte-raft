254c5a34e9b067db56b0d650bbc5908dfd90e9fc
Jacob Vallejo <you@jahkeup.com>
2014-07-16 14:42:13 -0400

    Fill in some tests for handlers

--- src/monte_raft/node/handlers.clj
+++ src/monte_raft/node/handlers.clj
@@ -14,16 +14,32 @@
    :confirm handle-confirm
    :commit handle-commit})
 
+(defmacro respond
+  ([msg]
+     `(socket/send-str-timeout socket/node-control-socket
+        socket/default-timeout ~msg))
+  ([sock msg]
+     `(socket/send-str-timeout ~sock
+        socket/default-timeout ~msg)))
+
 (defn handle-ping
-  "Handle a PING command" []
+  "Handle a PING command" [reply-sock]
   (let [response (first (msgs/responses-for :ping))]
-    (socket/send-str-timeout socket/control-socket socket/default-timeout)))
+    (respond reply-sock response)))
 
 (defn handle-elect
-  "Handle ELECT command" [])
+  "Handle ELECT command" [reply-sock])
 
 (defn handle-confirm
-  "Handle CONFIRM command" [])
+  "Handle CONFIRM command" [reply-sock]
+  (if (node-state/confirmable?)
+    (do (node-state/confirmed!)
+        (respond reply-sock :confirmed))
+    (respond reply-sock :resend)))
 
 (defn handle-commit
-  "Handle COMMIT command" [])
+  "Handle COMMIT command" [reply-sock]
+  (if (node-state/confirmed?)
+    (do (node-state/commit!)
+        (respond reply-sock :committed))
+    (respond reply-sock :retry)))
