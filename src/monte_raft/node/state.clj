4c11c62bc2a4b5251abdd72fc1d16f6d3ea444bd
Jacob Vallejo <you@jahkeup.com>
2014-07-16 14:42:47 -0400

    Add more algorithm stateful state objects in state

--- src/monte_raft/node/state.clj
+++ src/monte_raft/node/state.clj
@@ -16,3 +16,24 @@
 (def ^:dynamic global-nodes-state
   "YES BAD BAD BAD, we're using this for statstics." (atom {}))
 
+(def ^:dynamic heartbeat-failure
+  "Channel recieves message on a heartbeat failure." (atom false))
+
+(def ^:dynamic confirmed
+  "Has the transient state been confirmed?" (atom false))
+
+(defn confirmable? "Is the current state confirmable?" []
+  (not (nil? @transient-state)))
+
+(defn confirmed! "Mark transient state confirmed" []
+  (reset! confirmed true))
+
+(defn confirmed? "Has the current transient state been confirmed?" []
+  (true? @confirmed))
+
+(defn commit!
+  "Terminate state chain, update state and clear transient and
+  confirmation flag" []
+  (reset! state @transient-state)
+  (reset! confirmed false)
+  (reset! transient-state nil))
