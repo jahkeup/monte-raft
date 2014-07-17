fe50936fbcb69ef33f5fe649f4308aa5e323b86d
Jacob Vallejo <you@jahkeup.com>
2014-07-16 22:21:35 -0400

    Add a deftest-pending macro

--- test/monte_raft/utils.clj
+++ test/monte_raft/utils.clj
@@ -1,4 +1,5 @@
-(ns monte-raft.utils)
+(ns monte-raft.utils
+  (:require [clojure.test :refer :all]))
 
 (defmacro after-mseconds
   "Perform body after waiting for msecs"
@@ -6,3 +7,11 @@
   `(do
      (Thread/sleep ~msec)
      ~@body))
+
+;; http://techbehindtech.com/2010/06/01/marking-tests-as-pending-in-clojure/
+;; A Simple macro that enable to mark your test
+;; to pending
+(defmacro deftest-pending [name & body]
+ (let [message (str "\n========\n" name " is pending !!\n========\n")]
+   `(deftest ~name
+         (println ~message))))
