925cc95c1ed6f89abb48e7436084f6d55a8a742f
Jacob Vallejo <you@jahkeup.com>
2014-07-17 10:43:40 -0400

    Add a zmq version of the on-*-reset! macro

--- src/monte_raft/node/macros.clj
+++ src/monte_raft/node/macros.clj
@@ -1,5 +1,6 @@
 (ns monte-raft.node.macros
-  (:require [clojure.core.async :refer [go <! chan]]))
+  (:require [clojure.core.async :refer [go <! chan]]
+            [zeromq.zmq :as zmq]))
 
 (defmacro on-message-reset!
   "Upon recieving a message from channel, reset! the `set-atom' to
@@ -8,8 +9,22 @@
   `(go (do (<! ~channel)
            (reset! ~set-atom ~set-value))))
 
+(defmacro on-receive-reset!
+  "Upon receiving a message from a zmq socket, reset! the `set-atom'
+  to `set-value'"
+  [socket set-atom set-value]
+  `(go (do (zmq/receive-str ~socket)
+           (reset! ~set-atom ~set-value))))
+
 (defmacro until-message-from [message-chan & body]
   `(let [should-stop# (atom false)]
      (on-message-reset! ~message-chan should-stop# true)
      (while (not @should-stop#)
        ~@body)))
+
+(defmacro until-receive-from [socket & body]
+  `(let [should-stop# (atom false)]
+    (on-receive-reset! ~socket should-stop# true)
+    (while (not @should-stop#)
+      ~@body)))
+
