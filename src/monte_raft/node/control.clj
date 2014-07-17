73ca3ad38c1509e4ce07a6fc1e5d793cf4194350
Jacob Vallejo <you@jahkeup.com>
2014-07-17 19:41:24 -0400

    Add some meat to the control worker

--- src/monte_raft/node/control.clj
+++ src/monte_raft/node/control.clj
@@ -1,12 +1,17 @@
 (ns monte-raft.node.control
-  (:require [monte-raft.node.handlers :as handlers]
+  (:require [taoensso.timbre :as log]
+            [monte-raft.node.handlers :as handlers]
             [monte-raft.node.state :as node-state]
             [monte-raft.node.messaging :as msgs]
-            [monte-raft.node.worker-comm :refer :all]))
+            [monte-raft.node.socket :as socket]
+            [monte-raft.node.worker-comm :refer :all]
+            [monte-raft.node.macros :refer [until-message-from]]))
 
 (defn handle-message
-  "Inbound message dispatcher"
+  "Inbound message dispatcher, msg should be raw message receieved
+  from remotes"
   [reply-socket msg]
+  (log/tracef "Handling message: '%s'" msg)
   (if (msgs/valid-cmd? msg)
     (let [cmd (msgs/to-command-fmt msg)]
       (if (contains? handlers/cmd-handlers cmd)
@@ -14,10 +19,26 @@
           (handler-func reply-socket))))))
 
 (defn control-worker
-  "Go thread control worker to process incoming messages
+  "Go thread control worker to process incoming messages, must be
+  started prior to other workers
 
   node-id: the node's identifier
 
   control-binding: the binding of the control socket"
-  [control-binding]
-  (binding [worker-comm-sock (make-worker-comm-sock)]))
+  [control-binding term-chan]
+  (log/tracef "Starting control worker: listening on %s" control-binding)
+  (binding [worker-comm-sock (do (log/trace "Establishing worker communication socket")
+                                 (make-worker-comm-sock))
+            socket/control-socket (do (log/trace "Binding control socket")
+                                      (socket/make-control-listener control-binding))]
+    (log/trace "Beginning control worker loop")
+    (until-message-from term-chan
+      ;; Control loop
+      (log/trace "Gonna sleep...")
+      (Thread/sleep 1)
+      (log/trace "control-worker looping."))
+    (log/trace "Control socket is preparing to exit.")
+    (doall (for [w '(:leader :state)]
+             (do (log/tracef "Sending worker '%s' kill message" (name w))
+                 (send-worker-message w :terminate))))))
+
