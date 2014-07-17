88285e15a353c471c650cc3c417e218a7e5e6df1
Jacob Vallejo <you@jahkeup.com>
2014-07-17 19:41:53 -0400

    Add default and docstrings to client functions

--- src/monte_raft/client.clj
+++ src/monte_raft/client.clj
@@ -4,37 +4,54 @@
             [monte-raft.node.socket :as socket]))
 ;; Client interaction suite
 
-(def example-state {:name "Jake" :class "MATH430"})
+(def example-state "Simple internal representation of the state that
+  will be maintained by the raft cluster"
+  {:name "Jake" :class "MATH430"})
 
-(defn encode "Encode a state object (json wrapper)"
+(defn encode
+  "Encode a state object (json wrapper)"
   [state]
   (json/write-str state))
 
-(defn decode "Decode a state object from leader"
+(defn decode
+  "Decode a state object from leader"
   [state-str]
   (json/read-str state-str))
 
 (defn send-command
+  "Send command to system, if timeout is not given the
+  node.socket/default-timeout will be used."
   ([client-socket command]
      (send-command client-socket command socket/default-timeout))
   ([client-socket command timeout]
      (socket/send-str-timeout client-socket command timeout)))
 
-(defmacro with-client [client-sym leader-remote & body]
+(defmacro with-client
+  "Create a client socket bound on `client-sym'. Will connect to
+  leader, exec, and then close socket."
+  [client-sym leader-remote & body]
   `(with-open [~(symbol client-sym)
                (doto (zmq/socket socket/ctx :req)
                      (zmq/connect ~leader-remote))]
      ~@body))
 
-(defn update-state-msg [state]
-  (format "UPDATE %s" state))
+(defn update-state-msg
+  "Format an update message using the state object that should already
+  be represented by a json string"
+  [state]
+  (if (string? state)
+    (format "UPDATE %s" state)
+    (format "UPDATE %s" (encode state))))
 
-(defn update-state [leader-remote new-state]
-  (let [encoded-state (encode new-state)]
-    (with-client client leader-remote
-      (send-command client (update-state-msg encoded-state)))))
+(defn update-state
+  "Update the system consensus state to `new-state'"
+  [leader-remote new-state]
+  (with-client client leader-remote
+    (send-command client (update-state-msg new-state))))
 
-(defn get-state [leader-remote]
+(defn get-state
+  "Get the current state held in the system"
+  [leader-remote]
   (with-client client leader-remote
     (let [response (send-command client "GET")]
       (decode response))))
