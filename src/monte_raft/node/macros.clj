7b770644052d72f364aeb9aa49ffa7070db0db20
Jacob Vallejo <you@jahkeup.com>
2014-07-22 12:11:40 -0400

    on-*-reset! now captures value and return the value in a go chan

--- src/monte_raft/node/macros.clj
+++ src/monte_raft/node/macros.clj
@@ -5,21 +5,25 @@
 
 (defmacro on-message-reset!
   "Upon recieving a message from channel, reset! the `set-atom' to
-  `set-value'. Non-blocking"
+  `set-value'. Non-blocking, the resulting channel (from go) will pass
+  along the received value and exit."
   [channel set-atom set-value]
-  `(go (do (log/trace "Waiting for channel lock message")
-           (<! ~channel)
-           (log/trace "Received unlock message")
-           (reset! ~set-atom ~set-value))))
+  `(go (log/trace "Waiting for channel unlock message")
+       (let [received# (<! ~channel)]
+         (log/trace "Received unlock message")
+         (reset! ~set-atom ~set-value)
+         received#)))
 
 (defmacro on-receive-reset!
   "Upon receiving a message from a zmq socket, reset! the `set-atom'
-  to `set-value'"
+  to `set-value', the resulting channel (from go) will pass along the
+  received value and exit"
   [socket set-atom set-value]
-  `(go (do (log/trace "Waiting for socket unlock message on socket")
-           (zmq/receive-str ~socket)
-           (log/trace "Received message on socket")
-           (reset! ~set-atom ~set-value))))
+  `(go (log/trace "Waiting for socket unlock message on socket")
+       (let [received#(zmq/receive-str ~socket)]
+         (log/trace "Received message on socket")
+         (reset! ~set-atom ~set-value)
+         received#)))
 
 (defmacro until-message-from [message-chan & body]
   `(let [should-stop# (atom false)]
