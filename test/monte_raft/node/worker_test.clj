37a8594efe0db6e01ae8f588c5c8342d86f475a1
Jacob Vallejo <you@jahkeup.com>
2014-07-22 14:49:03 -0400

    Clean up generic worker test with deftest-worker

--- test/monte_raft/node/worker_test.clj
+++ test/monte_raft/node/worker_test.clj
@@ -1,5 +1,6 @@
 (ns monte-raft.node.worker-test
   (:require [clojure.test :refer :all]
+            [monte-raft.test.worker-macros :refer :all]
             [clojure.core.async :refer [<! <!! >! >!! go]]
             [monte-raft.node.worker :as worker]
             [monte-raft.node.state :as node-state]
@@ -8,17 +9,15 @@
 
 
 (binding [node-state/node-id "testing-node"]
-  (deftest test-worker-termination
-    (with-open [comm-sock (worker/make-comm-sock)]
-      (binding [worker/comm-sock comm-sock]
-        (let [running-worker (worker/start
-                               (worker/until-worker-terminate :test-worker
-                                 (Thread/sleep 100))
-                               :terminated)
-              received? (atom false)
-              value-received (on-message-reset! running-worker received? true)]
-          (Thread/sleep 100)
-          (worker/signal-terminate :test-worker)
-          (Thread/sleep 50)
-          (is received?)
-          (is (= (<!! value-received) :terminated)))))))
+  (deftest-worker test-worker-termination
+    (let [running-worker (worker/start
+                           (worker/until-worker-terminate :test-worker
+                             (Thread/sleep 100))
+                           :terminated)
+          received? (atom false)
+          value-received (on-message-reset! running-worker received? true)]
+      (Thread/sleep 100)
+      (worker/signal-terminate :test-worker)
+      (Thread/sleep 50)
+      (is received?)
+      (is (= (<!! value-received) :terminated)))))
