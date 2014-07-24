6d0d3e8ca9b637d41454c22bc750da24de48a772
Jacob Vallejo <you@jahkeup.com>
2014-07-24 06:59:53 -0400

    More correctly named, worker-config

--- src/monte_raft/node/state_worker.clj
+++ src/monte_raft/node/state_worker.clj
@@ -12,16 +12,16 @@
 (defn state-worker
   "Go thead designed to run in the background until a message is sent
   over the stop-chan channel. "
-  [{:keys [leader-publish-remote kill-codes] :as worker-options}]
+  [{:keys [leader-publish-remote kill-codes] :as worker-config}]
   (log/infof "Starting state-worker on '%s'" node-state/node-id)
   (with-open [update-socket (doto (zmq/socket socket/ctx :sub)
                               (zmq/connect leader-publish-remote)
                               (zmq/subscribe ""))]
-    (log/tracef "state-worker started.")
+    (log/tracef "state-worker started and connected to '%s'." leader-publish-remote)
     (worker/until-worker-terminate (kill-codes :state)
       (log/trace "waiting for state update...")
       ;; Potential problems here in the future if the windows don't align..
-      (if-let [new-state (maybe-get-update update-socket (worker-options :timeout))]
+      (if-let [new-state (maybe-get-update update-socket (worker-config :timeout))]
         (do (reset! node-state/transient-state new-state)
             (log/tracef "State updated: '%s'" new-state)))))
   (log/tracef "state-worker exiting.")
