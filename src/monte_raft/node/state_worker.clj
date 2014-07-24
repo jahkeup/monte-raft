7a530cb878131562c6362e3a8ba110d1263f9f0e
Jacob Vallejo <you@jahkeup.com>
2014-07-24 16:36:02 -0400

    Pull the leader-remote out in a more manageable way.

--- src/monte_raft/node/state_worker.clj
+++ src/monte_raft/node/state_worker.clj
@@ -2,6 +2,7 @@
   (:require [monte-raft.node.worker :as worker]
             [monte-raft.node.socket :as socket]
             [monte-raft.node.state :as node-state]
+            [monte-raft.node.leader-worker :as leader]
             [taoensso.timbre :as log]
             [zeromq.zmq :as zmq]))
 
@@ -12,17 +13,18 @@
 (defn state-worker
   "Go thead designed to run in the background until a message is sent
   over the stop-chan channel. "
-  [{:keys [leader-publish-remote kill-codes] :as worker-config}]
+  [{:keys [kill-codes] :as worker-config}]
   (log/infof "Starting state-worker on '%s'" node-state/node-id)
-  (with-open [update-socket (doto (zmq/socket socket/ctx :sub)
-                              (zmq/connect leader-publish-remote)
-                              (zmq/subscribe ""))]
-    (log/tracef "state-worker started and connected to '%s'." leader-publish-remote)
-    (worker/until-worker-terminate (kill-codes :state)
-      (log/trace "waiting for state update...")
-      ;; Potential problems here in the future if the windows don't align..
-      (if-let [new-state (maybe-get-update update-socket (worker-config :timeout))]
-        (do (reset! node-state/transient-state new-state)
-            (log/tracef "State updated: '%s'" new-state)))))
+  (let [leader-publish-remote (leader/leader-remote worker-config)]
+    (with-open [update-socket (doto (zmq/socket socket/ctx :sub)
+                                (zmq/connect leader-publish-remote)
+                                (zmq/subscribe ""))]
+      (log/tracef "state-worker started and connected to '%s'." leader-publish-remote)
+      (worker/until-worker-terminate (kill-codes :state)
+        (log/trace "waiting for state update...")
+        ;; Potential problems here in the future if the windows don't align..
+        (if-let [new-state (maybe-get-update update-socket (worker-config :timeout))]
+          (do (reset! node-state/transient-state new-state)
+              (log/tracef "State updated: '%s'" new-state))))))
   (log/tracef "state-worker exiting.")
   :terminated)
