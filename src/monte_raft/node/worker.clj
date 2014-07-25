ffffc3c8180a595ffeef05b86423ce8bf876b62e
Jacob Vallejo <you@jahkeup.com>
2014-07-24 21:42:53 -0400

    Overhaul worker comm-sock scheme to allow for binding at any level

--- src/monte_raft/node/worker.clj
+++ src/monte_raft/node/worker.clj
@@ -6,7 +6,8 @@
             [zeromq.zmq :as zmq]
             [taoensso.timbre :as log]))
 
-(def ^:dynamic comm-sock nil)
+(def ^:dynamic comm-sock
+  "Socket to be used for publishing worker messages" nil)
 
 (defmacro log-error-throw "Log an error and throw it"
   [msg]
@@ -15,25 +16,41 @@
 
 (defn comm-remote
   "Generate the inproc control worker communication socket address
-  based on the current binding of the node-id" []
-  (format "inproc://%s-worker-comm" node-state/node-id))
+  based on the current binding of the node-id (or pull from a
+  node-config map passed"
+  [node-id]
+  (let [conn-string "inproc://%s-worker-comm"
+        conn-remote (cond
+                      (map? node-id) (format conn-string (name (:node-id node-id)))
+                      :default (format conn-string (name node-id)))]
+    conn-remote))
 
 (defn make-comm-sock
-  "Create a worker communication socket"
-  ([]
-     (make-comm-sock (comm-remote)))
-  ([sock-binding]
-     (doto (zmq/socket socket/ctx :pub)
-       (zmq/bind sock-binding))))
+  "Create a worker communication socket, refuses to create double sockets"
+  [{:keys [node-id]}]
+     (if comm-sock comm-sock
+         (do (log/tracef "Worker creating new worker pub '%s'" (comm-remote node-id))
+           (doto (zmq/socket socket/ctx :pub)
+            (zmq/bind (comm-remote node-id))))))
+
+(defmacro with-comm-sock
+  "Bind comm-sock and run"
+  [node-config & body]
+  `(binding [comm-sock (make-comm-sock ~node-config)]
+     ~@body))
 
 (defmacro until-worker-terminate
   "Loop until a message arrives on a subscribed worker socket"
-  [worker-type & body]
-  `(let [sock# (doto (zmq/socket socket/ctx :sub)
-                 (zmq/connect (comm-remote))
-                 (zmq/subscribe (name ~worker-type)))]
-     (log/tracef "Worker subscribing to '%s' for messages" (name ~worker-type))
-     (until-receive-from sock# ~@body)))
+  [worker-config worker-type & body]
+  `(do
+     (let [worker-sub-name# (get-in ~worker-config [:kill-codes ~worker-type] ~worker-type)
+           _# (log/tracef "Worker (%s) connecting to '%s' as worker comm"
+               (name worker-sub-name#) (comm-remote ~worker-config))
+           sock# (doto (zmq/socket socket/ctx :sub)
+                  (zmq/connect (comm-remote ~worker-config))
+                  (zmq/subscribe (name worker-sub-name#)))]
+      (log/tracef "Worker subscribing to '%s' for messages" (name worker-sub-name#))
+      (until-receive-from sock# ~@body))))
 
 (defn comm-sock-logger [remote]
   (with-open [sock (doto (zmq/socket socket/ctx :sub)
@@ -48,6 +65,7 @@
   subscribers and if subscribed with until-worker-terminate this will
   cause remote worker to cease looping."
   [worker-type msg]
+  (if (not comm-sock) (throw (Exception. "Worker communication socket has not been opened.")))
   (zmq/send-str comm-sock
     (format "%s %s" (name worker-type) msg)))
 
