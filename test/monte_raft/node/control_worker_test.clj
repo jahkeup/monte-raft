33fea7b37785e1fedbbad15cac4bd430938a1d91
Jacob Vallejo <you@jahkeup.com>
2014-07-26 16:41:58 -0400

    Test control-worker using the nested state and chan signalling

--- test/monte_raft/node/control_worker_test.clj
+++ test/monte_raft/node/control_worker_test.clj
@@ -1,6 +1,6 @@
 (ns monte-raft.node.control-worker-test
   (:require [clojure.test :refer :all]
-            [clojure.core.async :refer [<!!]]
+            [clojure.core.async :refer [<!! chan]]
             [zeromq.zmq :as zmq]
             [monte-raft.utils :refer :all]
             [monte-raft.node.socket :as socket]
@@ -9,6 +9,21 @@
             [monte-raft.node.handlers :as handlers]
             [monte-raft.test.worker-macros :refer :all]
             [taoensso.timbre :as log]))
+
+(defn clear-state! [state-map]
+  (doall
+    (for [[k resetval] [[:transient nil] [:current nil]
+                        [:term 0]]]
+      (reset! (get state-map k) resetval))))
+
+(defmacro testing-clean-state [state desc & body]
+  `(testing ~desc
+     (do (clear-state! ~state)
+         ~@body)))
+
+(defn send-recv-timeout [sock send]
+  (socket/send-str-timeout sock send)
+  (socket/receive-str-timeout sock))
 
 (deftest test-handle-message
   (testing "Calls handler when matches message"
@@ -22,6 +37,34 @@
   (let [running-worker (worker/start
                          (control/control-worker (node-config)))]
     (wait-do 100
-      (worker/signal-terminate (get-in (node-config) [:kill-codes :control]))
+      (worker/signal-terminate :control (node-config))
       (is (= (<!! running-worker) :terminated)))))
+
+(deftest-worker test-control-reactions
+  (let [{:keys [state node-id control-binding kill-codes] :as worker-config} (node-config)
+        {:keys [transient current term]} state
+        log-val (fn [desc x] (log/tracef "Current value of '%s': '%s'" desc x))
+        started-chan (chan 1)
+        running-worker (worker/start (control/control-worker worker-config
+                                       started-chan))]
+    (log/tracef "Test will begin once worker has started.")
+    (or (not (= (<!! started-chan) :control-fail))
+      (throw (Exception. "Control did not start.")))
+    (try
+      (with-open [sock (doto (zmq/socket socket/ctx :req)
+                         (zmq/connect control-binding))]
+        (log/infof "Started control-worker (%s) for testing, will shutdown after.%s"
+          (:control kill-codes) bar)
+        (testing-clean-state state
+          "Control should respond with a confirm if transient state is present"
+          (reset! transient "some-state")
+          (if-let [response (send-recv-timeout sock "CONFIRM")]
+            (is (= response "CONFIRMED"))))
+        (testing-clean-state state
+          "Control should set current upon a COMMIT and respond"
+          (let [response (send-recv-timeout sock "COMMIT")]
+            (is (= response "COMMITTED"))))))
+    (worker/signal-terminate :control worker-config)
+    (<!! running-worker)
+    (log/infof "Test subject has been shutdown.%s" bar)))
 
